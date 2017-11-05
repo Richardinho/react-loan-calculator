189ae09decd4d5a5c8eba96e6dcb5a4ec8074e62
Richard Hunter <richard@richardhunter.co.uk>
2017-11-05 16:51:14 +0000

    testing for header in input field

--- test/foo.spec.js
+++ test/foo.spec.js
@@ -1,8 +1,7 @@
-import Foo from '../src/foo.js';
 import React from 'react';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import NumberFormat from 'react-number-format';
 configure({ adapter: new Adapter() });
 
 import AmountInputField from '../src/amount-input-field.jsx';
@@ -10,8 +9,8 @@ import { mount, shallow } from 'enzyme';
 
 describe('foo', () => {
   it('should..', () => {
-    let foo = new Foo();
-    expect(foo.bar()).toBe('this is bar');
     const wrapper = shallow(<AmountInputField/>);
+    expect(wrapper.find('h3').length).toBe(1);
+    expect(wrapper.find('h3').text()).toBe('How much do you want to borrow?');
   });
 });
